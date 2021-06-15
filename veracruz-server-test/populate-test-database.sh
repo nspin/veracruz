fc194a0af76bca6bd4922e7e20fba6e3e49ccb87
Nick Spinale <nick@nickspinale.com>
2021-06-15 02:34:53 +0000

    x

--- veracruz-server-test/populate-test-database.sh
+++ veracruz-server-test/populate-test-database.sh
@@ -4,7 +4,5 @@ rm -f proxy-attestation-server.db
 diesel --config-file ../proxy-attestation-server/diesel.toml setup
 echo "INSERT INTO firmware_versions VALUES(1, 'sgx', '0.3.0', '${hash_value}');" > tmp.sql
 echo "INSERT INTO firmware_versions VALUES(2, 'psa', '0.3.0', 'deadbeefdeadbeefdeadbeefdeadbeeff00dcafef00dcafef00dcafef00dcafe');" >> tmp.sql
-pcr0=`cat ../nitro-root-enclave/PCR0`
-echo "INSERT INTO firmware_versions VALUES(3, 'nitro', '0.1.0', '${pcr0}');" >> tmp.sql
 sqlite3 proxy-attestation-server.db < tmp.sql
 
