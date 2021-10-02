47021a7b7b609bf47e953942db93a65a579781d0
Nick Spinale <nick@nickspinale.com>
2021-10-02 16:38:37 +0000

    x

--- icecap/nix/instance.nix
+++ icecap/nix/instance.nix
@@ -32,8 +32,7 @@ in lib.fix (self: with self; {
   run = platUtils.${icecapPlat}.bundle {
     firmware = icecapFirmware.image;
     payload = icecapFirmware.mkDefaultPayload {
-      # linuxImage = pkgs.linux.icecap.linuxKernel.host.${icecapPlat}.kernel;
-      linuxImage = ../../../local/linux/arch/arm64/boot/Image;
+      linuxImage = pkgs.linux.icecap.linuxKernel.host.${icecapPlat}.kernel;
       initramfs = hostUser.config.build.initramfs;
       bootargs = [
         "earlycon=icecap_vmm"
